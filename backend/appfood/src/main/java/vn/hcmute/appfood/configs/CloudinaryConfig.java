e6ad24e1acde5e330ccee7ed20414abbe67e2f69
NhatNguyen1784 <nguyennhatnguyen1782004@gmail.com>
2025-03-24 16:54:05 +0700

    fix drawable android

---
+++ backend/appfood/src/main/java/vn/hcmute/appfood/configs/CloudinaryConfig.java
@@ -0,0 +1,17 @@
+package vn.hcmute.appfood.configs;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "AppFood",
+                "api_key", "585447145779693",
+                "api_secret","rnN5GjxZLti0uCZv_gkHNXV3CPw"));
+    }
+}
