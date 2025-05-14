b1feee91819c31d278b6b39635a0c180ac93317b
DKNguyen13 <nd2004lk13@gmail.com>
2025-05-14 13:21:24 +0700

    Add isReview attribute to OrderDetail entity

--- android/app/src/main/java/vn/hcmute/appfoodorder/ui/adapter/OrderDetailAdapter.java
+++ android/app/src/main/java/vn/hcmute/appfoodorder/ui/adapter/OrderDetailAdapter.java
@@ -1,11 +1,16 @@
 package vn.hcmute.appfoodorder.ui.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
@@ -14,21 +19,24 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import java.util.List;
 import vn.hcmute.appfoodorder.R;
 import vn.hcmute.appfoodorder.model.dto.response.OrderDetailDTO;
+import vn.hcmute.appfoodorder.ui.activity.order.OrderStatusActivity;
 
 public class OrderDetailAdapter extends RecyclerView.Adapter<OrderDetailAdapter.OrderDetailViewHolder> {
 
     private Context context;
     private List<OrderDetailDTO> orderDetails;
+    private boolean isSuccessful = false;
 
-    public OrderDetailAdapter(Context context, List<OrderDetailDTO> orderDetails) {
+    public OrderDetailAdapter(Context context, List<OrderDetailDTO> orderDetails, boolean isSuccessful) {
         this.context = context;
         this.orderDetails = orderDetails;
+        this.isSuccessful = isSuccessful;
     }
 
     @NonNull
     @Override
     public OrderDetailViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_food_order, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_order_details, parent, false);
         return new OrderDetailViewHolder(view);
     }
 
@@ -41,6 +49,23 @@ public class OrderDetailAdapter extends RecyclerView.Adapter<OrderDetailAdapter.
                 .into(holder.imageFood);
         holder.tvFoodName.setText(item.getQuantity() + " x "+ item.getFoodName());
         holder.tvTotalPrice.setText(String.format("Giá %,.0f đ", item.getUnitPrice()));
+        if(isSuccessful && item.getReview() == false){
+            holder.btnRatingOrderDetail.setVisibility(View.VISIBLE);
+            holder.btnRatingOrderDetail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /*
+                    if (context instanceof Activity) {
+                        Intent intent = new Intent(context, );
+                        intent.putExtra("oddId", item.getId());
+                        ((Activity) context).finish();
+                        context.startActivity(intent);
+                    }
+                     */
+                    Toast.makeText(context, "Orderdetail id "+item.getId(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
     @Override
@@ -51,11 +76,13 @@ public class OrderDetailAdapter extends RecyclerView.Adapter<OrderDetailAdapter.
     public class OrderDetailViewHolder extends RecyclerView.ViewHolder{
         private ImageView imageFood;
         private TextView tvFoodName, tvTotalPrice;
+        private Button btnRatingOrderDetail;
         public OrderDetailViewHolder(@NonNull View itemView) {
             super(itemView);
             imageFood = itemView.findViewById(R.id.imgFoodOrder);
             tvTotalPrice = itemView.findViewById(R.id.txtFoodPriceOrder);
             tvFoodName = itemView.findViewById(R.id.txtFoodNameOrder);
+            btnRatingOrderDetail = itemView.findViewById(R.id.btnRatingOrderDetail);
         }
     }
 }
